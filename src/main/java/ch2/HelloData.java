278c49e7801dd4a26f6eea9ac93c3491341ffb47
navy <sdn_app@163.com>
2020-07-09 10:17:04 +0800

    Initial commit

---
+++ src/main/java/ch2/HelloData.java
@@ -0,0 +1,10 @@
+package ch2;
+
+import java.util.Date;
+
+public class HelloData {
+    public static void main(String[] args) {
+        System.out.println("Hello , it's :  ");
+        System.out.println(new Date());
+    }
+}
